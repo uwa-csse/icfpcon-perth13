e78d5326eedb748906f4edc0a6885774e9792e5d
Rowan Davies <rd1rd2@gmail.com>
2013-08-10 03:17:00 +0800

    Modified gen-eval-test-io to run hbv.
    Also added 20 training fetches for sizes 6 and 8-20  (20 of size 7 were added previously).

--- Glue/gen-eval-test-io.py
+++ Glue/gen-eval-test-io.py
@@ -1,6 +1,7 @@
 import random
 import os
 import sys
+import subprocess
 
 from server import Server
 
@@ -19,14 +20,35 @@ inputs = []
 while len(inputs) < 256: inputs.append(random.getrandbits(64))
 outputs = s.evaluate(problem = prob['id'], arguments = inputs)
 
-if not os.path.exists("tests"):
-	os.makedirs("tests")
+if not os.path.exists("trains"):
+	os.makedirs("trains")
 
-in_file = open("tests/%s.in" % prob['id'], 'w')
-in_file.write("%s\n" % prob['challenge'])
-for i in inputs:
-	in_file.write("%d\n" % i)
+############
+opstr1 = "_".join(prob['operators'])
+opstr2 = " ".join(prob['operators'])
 
-out_file = open("tests/%s.expected" % prob['id'], 'w')
-for o in outputs:
-	out_file.write("%d\n" % o)
+inout_fname = "trains/%02d-%s-%s.inout" % (prob['size'], opstr1, prob['id'])
+inout_file = open(inout_fname, 'w')
+inout_file.write("# %s\n" % prob['challenge'])
+
+inout_file.write("%d %s\n" % (prob['size'], opstr2))
+
+inouts = zip(inputs, outputs)
+
+for (i,o) in inouts:
+   inout_file.write("%d %d\n" % (i,o))
+
+inout_file.close()
+
+#############
+myinput = open(inout_fname)
+myoutput = open(inout_fname + ".hbv", 'w')
+hbv_out = subprocess.check_output(['../hbv/dist/build/hbv/hbv', '-b'], stdin=myinput)
+
+print hbv_out
+
+myoutput.write(hbv_out)
+
+#myoutput.flush()
+
+###########
