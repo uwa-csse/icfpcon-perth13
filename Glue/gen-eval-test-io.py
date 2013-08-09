6a6d8e6fa9236400063059e8bf50cee05cfd0a20
BudgieInWA <benjaminaritter@gmail.com>
2013-08-09 20:12:17 +0800

    Updated the test fetching script.

--- Glue/gen-eval-test-io.py
+++ Glue/gen-eval-test-io.py
@@ -5,28 +5,28 @@ import sys
 from server import Server
 
 
-if (sys.argv < 3):
-	print "usage: %s <size> <op> [<op> [...]]", sys.arg[0]
+if (len(sys.argv) != 2):
+	print "usage: %s <size>", sys.argv[0]
 	exit (1)
 
-size = sys.argv[1]
-operators = sys.argv[2:]
+sz = sys.argv[1]
 
 s = Server()
-prob = s.get_training_problem(size = size, operators = operators)
+prob = s.get_training_problem(int(sz))
+print prob["id"]
 
 inputs = []
 while len(inputs) < 256: inputs.append(random.getrandbits(64))
-outputs = s.evaluate(problem = prob.id, arguments = inputs)
+outputs = s.evaluate(problem = prob['id'], arguments = inputs)
 
 if not os.path.exists("tests"):
 	os.makedirs("tests")
 
-in_file = open("tests/%s.in" % prob.id, 'w')
-in_file.write("%s\n" % prob.challenge)
+in_file = open("tests/%s.in" % prob['id'], 'w')
+in_file.write("%s\n" % prob['challenge'])
 for i in inputs:
 	in_file.write("%d\n" % i)
 
-out_file = open("tests/%s.expected" % prob.id, 'w')
+out_file = open("tests/%s.expected" % prob['id'], 'w')
 for o in outputs:
 	out_file.write("%d\n" % o)
